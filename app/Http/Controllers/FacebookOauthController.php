8f96b4d0198c2570a9ab5d759aa897b688529da3
netsage <netsage23@gmail.com>
2020-01-14 19:06:23 +0000

    updated by netsage

---
+++ app/Http/Controllers/FacebookOauthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\User;
+use Validator;
+use Socialite;
+
+
+class FacebookOauthController extends Controller
+{
+
+ /**
+     * Redirect the user to the facebook authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('facebook')->stateless()->redirect();
+    }
+
+    /**
+     * Obtain the user information from Facebook.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        // enter redirect url here
+        $url = "http://localhost:8000";
+
+        $user = Socialite::driver('facebook')->stateless()->user();
+        if($user->token){
+        $checkIfUserPresentInDb = User::where('email',$user->getEmail())->first();
+        if($checkIfUserPresentInDb){
+        // if user already exists
+        $token = $checkIfUserPresentInDb->createToken('authToken')->accessToken;
+        return redirect()->to($url.'?userId='.$checkIfUserPresentInDb->id.'token='.$token);
+        }else{
+            // if user does not exists
+         $createUser = User::create([
+             'name'=>$user->getName(),
+             'email'=>$user->getEmail(), 
+         ]);
+         $accessToken = $createUser->createToken('authToken')->accessToken;
+         return redirect()->to($url.'?token='.$accessToken.'userId='.$createUser->id); //redirect to users dashboard;
+        }
+        }
+    }
+
+
+}
