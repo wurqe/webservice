066c1bacdc1d793ad6ad7072c7122fd238eef246
myckhel <myckhel1@hotmail.com>
2020-01-14 17:01:48 +0100

    created oauth access token events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Laravel\Passport\Events\AccessTokenCreated;
+use Laravel\Passport\Events\RefreshTokenCreated;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,13 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        AccessTokenCreated::class => [
+          'App\Listeners\RevokeOldTokens',
+        ],
+
+        RefreshTokenCreated::class => [
+          'App\Listeners\PruneOldTokens',
+        ]
     ];
 
     /**
