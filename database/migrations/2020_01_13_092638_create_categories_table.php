f6f5b24b81d9124d20a9122bf28c9a84466817d4
myckhel <myckhel1@hotmail.com>
2020-01-15 15:29:03 +0100

    created import default db data command

--- database/migrations/2020_01_13_092638_create_categories_table.php
+++ database/migrations/2020_01_13_092638_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Category;
 
 class CreateCategoriesTable extends Migration
 {
@@ -23,6 +24,17 @@ class CreateCategoriesTable extends Migration
     Schema::table('categories', function (Blueprint $table) {
       $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
      });
+
+    //  $categories = ['Cleaning Service','Child Care','Elderly Care','Electrician','Food Truck','Caterer','Baker','Gardener','Landscaping Service','Pet grooming Service','Home Staging','Home Painting','Handyman','Printing','Direct Mailer','Personal Shopper','Event Planner','Errand Service','Food Delivery','Florist','Appliance Repair','Coaching ','Courier',
+    //   'Learning','Decoration','Disposal','Plumbing','DJing','Furniture / Design/Assembly/ Removal','Academic Writing','Tutor','Dog Walker','Mobile Retail Boutique','Car Wash ',' Farming','Tour Guide','Beautify','Barber ','Security Service','Artist','Home decor','Home Chores ','Massage Therapy','Personal Trainer','Dance Classes','Music Lessons','Moving Service',
+    //   'Tax Preparation','Hair Salon - ladies and gents','Hair Stylist','Health Care','Photography/Videography ','Research','Legal Practitioner','Local Job','Achitech ','Reseller','Retail Sales','Scripting','Shipping','Engineering','Training','Voice Artist'
+    // ];
+    //
+    // foreach ($categories as $cat) {
+    //   Category::create([
+    //     'name' => $cat,
+    //   ]);
+    // }
   }
 
   /**
